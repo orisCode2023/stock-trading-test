655f3c00accd4f5fbab3ad826cb1459d561beee4
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-11 12:56:28 +0200

    finish

--- utils/stockManager.js
+++ utils/stockManager.js
@@ -17,48 +17,46 @@ function updateStockPrice(operation, stock){
     if (operation === "buy"){
         stock.previousPrices.push(stock.currentPrice)
         stock.currentPrice += calcPercent(percentSingleStock, stock.currentPrice)
+        console.log(`The stock price went up from ${stock.previousPrices[stock.previousPrices.length - 1]} to ${stock.currentPrice}`)
+        updatePriceCatagoryAdd(stock)
     } else {
         stock.previousPrices.push(stock.currentPrice)
         stock.currentPrice -= calcPercent(percentSingleStock, stock.currentPrice)
+        console.log(`The stock price went down from ${stock.previousPrices[stock.previousPrices.length - 1]} to ${stock.currentPrice}`)
+        updatePriceCatagorySub(stock)
     }
 }
 
 function updateTime(){
     stockMarket["lastUpdated"] = new Date()
-    console.log(stockMarket.lastUpdated)
 }
 
-function OperateOnStock(operation, identifier) {
+
+function updatePriceCatagoryAdd(stock){
+    const percentCatagoryStock = 1
+    const stockCatagory = stockMarket.stocks.filter(stock1 => stock1.category === stock.category )
+    for (let i = 0; i < stockCatagory.length; i++){
+        stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
+        stockCatagory[i].currentPrice += calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
+    }
+     console.log(`The stock catagory price went up from ${stock.previousPrices[stock.previousPrices.length - 1]} to ${stock.currentPrice}`)
+}
+function updatePriceCatagorySub(stock){
+    const percentCatagoryStock = 1
+    const stockCatagory = stockMarket.stocks.filter(stock1 => stock1.category === stock.category )
+    for (let i = 0; i < stockCatagory.length; i++){
+        stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
+        stockCatagory[i].currentPrice -= calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
+    }
+    console.log(`The stock catagory price went down from ${stock.previousPrices[stock.previousPrices.length - 1]} to ${stock.currentPrice}`)
+}
+
+function operateOnStock(operation, identifier) {
     validOperations(operation)
     const stock = searchStock(identifier)
     const amount = input(`How many stocks do you want to ${operation} ?  `)
     stock.availableStocks -= isAvailable(amount, stock.availableStocks)
-    updateStockPrice(operation, stock)
-    // updateCatagoryPrice(stock.category)
+    updateStockPrice(operation, stock[0])
     updateTime()
 }
-
-
-console.log(stockMarket.stocks[0])
-updateStockPrice("sell", stockMarket.stocks[0])
-console.log(stockMarket.stocks[0])
-
-
-function updatePriceCatagory(operation, stock){
-    const percentCatagoryStock = 1
-    const stockCatagory = stockMarket.stocks.filter(stock1 => stock1.category === stock.category )
-    if (operation === "buy"){
-        for (let i = 0; i < stockCatagory.length; i++){
-            stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
-            stockCatagory[i].currentPrice += calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
-        }
-    } else {
-        for (let i = 0; i < stockCatagory.length; i++){
-            stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
-            stockCatagory[i].currentPrice -= calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
-        }
-    }
-    console.log(stockCatagory)
-}
-
-
+operateOnStock("buy", "BrightFuture Academy")
