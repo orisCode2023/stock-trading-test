acf9d9a7dc3a75e1e41d81f5d11cd62faa7ba689
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-11 10:12:57 +0200

    importing the stock data and creating a search function

--- utils/stockManager.js
+++ utils/stockManager.js
@@ -1,3 +1,11 @@
-function searchStock(identifier) {}
+import { stockMarket } from "../data/dataStock.js"
+
+const searchStock = (identifier) => {
+    const found = stockMarket.stocks.filter(stock => stock.name === identifier || stock.id === identifier )
+    if (found.length > 0) {
+        return found
+    }
+    return "Sorry, could not find this stock"
+}
 function filterStocksByPrice(givenPrice, above) {}
 function OperateOnStock(operation, identifier) {}
