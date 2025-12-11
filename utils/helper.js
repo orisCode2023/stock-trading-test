9c55dc07f46197a708c550ac34927dee58183241
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-11 12:39:58 +0200

    adding ffunction and helper function to buy and sell stocks and update time

--- utils/helper.js
+++ utils/helper.js
@@ -15,3 +15,30 @@ export function isEmpty(found){
         return found
     }
 }
+
+export function isAvailable(amount, storeAmount){
+    if (amount > storeAmount){
+        console.log(`There is only ${storeAmount} stocks available `)
+        return storeAmount
+    } 
+    return amount
+}
+
+export function validOperations(operation){
+    const operations = ["buy", "sell"]
+    while (!operations.includes(operation)) {
+        console.log("This is not an operation")
+        operation = input("Plaes enter operation again: ")
+    }
+} 
+
+export function isProperty(stock, identifier){
+    while (!stock.hasOwnProperty(identifier)) {
+        console.log("This is not a property")
+        identifier = input("Plaes enter identifier again: ")
+    }
+}
+
+export function calcPercent(percent, total){
+    return ((percent/ 100) * total)
+}
--- utils/stockManager.js
+++ utils/stockManager.js
@@ -1,10 +1,8 @@
+import input from "analiza-sync"
 import { stockMarket } from "../data/dataStock.js"
-import { isNumber, isEmpty } from "./helper.js"
+import { isNumber, isEmpty, isAvailable, validOperations, calcPercent } from "./helper.js"
 
-const searchStock = (identifier) => {
-    const found = stockMarket.stocks.filter(stock => stock.name === identifier || stock.id === identifier )
-    return isEmpty(found)
-}
+const searchStock = (identifier) => isEmpty(stockMarket.stocks.filter(stock => stock.name === identifier || stock.id === identifier ))
 
 function filterStocksByPrice(givenPrice, above) {
     isNumber(givenPrice)
@@ -13,4 +11,54 @@ function filterStocksByPrice(givenPrice, above) {
     } return isEmpty(stockMarket.stocks.filter(stock => stock.currentPrice < givenPrice))
 }
 
-function OperateOnStock(operation, identifier) {}
+
+function updateStockPrice(operation, stock){
+    const percentSingleStock = 5
+    if (operation === "buy"){
+        stock.previousPrices.push(stock.currentPrice)
+        stock.currentPrice += calcPercent(percentSingleStock, stock.currentPrice)
+    } else {
+        stock.previousPrices.push(stock.currentPrice)
+        stock.currentPrice -= calcPercent(percentSingleStock, stock.currentPrice)
+    }
+}
+
+function updateTime(){
+    stockMarket["lastUpdated"] = new Date()
+    console.log(stockMarket.lastUpdated)
+}
+
+function OperateOnStock(operation, identifier) {
+    validOperations(operation)
+    const stock = searchStock(identifier)
+    const amount = input(`How many stocks do you want to ${operation} ?  `)
+    stock.availableStocks -= isAvailable(amount, stock.availableStocks)
+    updateStockPrice(operation, stock)
+    // updateCatagoryPrice(stock.category)
+    updateTime()
+}
+
+
+console.log(stockMarket.stocks[0])
+updateStockPrice("sell", stockMarket.stocks[0])
+console.log(stockMarket.stocks[0])
+
+
+function updatePriceCatagory(operation, stock){
+    const percentCatagoryStock = 1
+    const stockCatagory = stockMarket.stocks.filter(stock1 => stock1.category === stock.category )
+    if (operation === "buy"){
+        for (let i = 0; i < stockCatagory.length; i++){
+            stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
+            stockCatagory[i].currentPrice += calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
+        }
+    } else {
+        for (let i = 0; i < stockCatagory.length; i++){
+            stockCatagory[i].previousPrices.push(stockCatagory[i].currentPrice)
+            stockCatagory[i].currentPrice -= calcPercent(percentCatagoryStock, stockCatagory[i].currentPrice)
+        }
+    }
+    console.log(stockCatagory)
+}
+
+
